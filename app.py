ad52b8181ee74fb78727961ed11b39263e790a0c
Abhay Maurya <abhaymaurya821@gmail.com>
2026-08-21 12:48:29 +0530

    feat: implement user authentication with login, dashboard, and logout functionality

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, session, make_response, redirect
 
 app = Flask(__name__)
 
+app.secret_key = "dfvsadknfoenrfcoaef"
 
+# Home
 @app.route("/")
-def home():
-    return render_template("index.html")
+def index():
+
+    username = request.cookies.get("username")
+
+    return render_template("index.html", username = username )
 
 
 @app.route("/about")
@@ -16,14 +21,67 @@ def about():
 def contact():
     return render_template("contact.html")
 
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    username = None
 
-    if request.method == "POST":
-        username = request.form.get("username")
+    if  request.method == 'POST':
 
-    return render_template("login.html", username=username)
+        username = request.form["username"]
+
+        # store username in session
+        session['username'] = username
+
+        # create response
+        response = make_response(
+            redirect(url_for('dashboard'))
+        )
+
+        response.set_cookie(
+            "username",
+            username,
+            max_age=60* 60* 24*7
+        )
+
+        return response
+
+    return render_template('login.html')
+
+
+# Dashboard
+@app.route("/dashboard")
+def dashboard():
+
+    if "username"  not in session:
+        return redirect(url_for('login'))
+
+    username = session["username"]
+
+    return render_template(
+        "dashboard.html",
+        username = username
+    )
+
+# logout
+@app.route("/logout")
+def logout():
+
+    # remove the username from session
+    session.pop("username", None)
+
+    return redirect(url_for("index"))
+
+# Delete cookie
+@app.route("/delete-cookie")
+def delete_cookie():
+
+    response = make_response(
+        redirect(url_for("index"))
+    )
+
+    response.delete_cookie("username")
+
+    return response
 
 
 if __name__ == "__main__":
